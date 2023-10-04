bea0d19d136bc8c47f118da0abc136b605147796
PavelOsw <pavel.osw117@gmail.com>
2023-10-03 22:24:35 -0600

    [WIP] general configuration modified

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { MenubarModule } from 'primeng/menubar';
+
+
 
 
 @NgModule({
-  declarations: [],
-  imports: [
+  exports: [
+    MenubarModule,
   ]
 })
 export class PrimeNgModule { }
