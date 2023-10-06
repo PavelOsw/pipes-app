ea7e696647e15a1044251f900a4dd57a3705286e
PavelOsw <pavel.osw117@gmail.com>
2023-10-06 00:21:10 -0600

    [WIP] Add Toolbar & Table modules for import

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,7 +5,8 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 
 
@@ -16,6 +17,8 @@ import { PanelModule } from 'primeng/panel';
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    ToolbarModule,
+    TableModule,
   ]
 })
 export class PrimeNgModule { }
